418e67eeb698af70c95c61eb3c950c6ae4491289
Shafayat1004 <shafayat1004@live.com>
2022-01-07 22:28:27 +0600

    Changed title

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -62,7 +62,7 @@
     }
   
     function initNavBar() {
-      return fetch("https://shafayat1004.github.io/My-Knowledge-Base/nav.html").then(function (response) {
+      return fetch("https://shafayat1004.github.io/My-Notes/nav.html").then(function (response) {
         return response.text().then((content) => {
           replaceNav(content);
           return;
@@ -80,7 +80,7 @@
     function initSearch() {
       var request = new XMLHttpRequest();
       // TODO: CHECK IF THIS WORKS WITHOUT ABSOLUTE_URL
-      request.open("GET", "https://shafayat1004.github.io/My-Knowledge-Base/assets/js/search-data.json", true);
+      request.open("GET", "https://shafayat1004.github.io/My-Notes/assets/js/search-data.json", true);
   
       request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
@@ -514,7 +514,7 @@
       var cssFile = document.querySelector('[rel="stylesheet"]');
       cssFile.setAttribute(
         "href",
-        'https://shafayat1004.github.io/My-Knowledge-Base/assets/css/just-the-docs-' + theme + ".css"
+        'https://shafayat1004.github.io/My-Notes/assets/css/just-the-docs-' + theme + ".css"
       );
     };
   
